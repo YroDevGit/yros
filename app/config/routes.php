ba35d99c9de0b8b444a902026d3ba677c7dc5bce
YroDevGit <tyronemalocon@gmail.com>
2024-09-15 18:08:40 +0800

    add routes update

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,14 @@
-<?php
+<?php 
 
-$routes['default'] = "main/index";
+
+$routes['default'] = "main/index"; //Web app main page/controller
 
 $routes["page_not_found"] = "main/page_not_found";
 $routes["error"] = "main/error_page";
 
 // add routes below, copy this sample: $routes[""] = "";
+
+
 
 
 
