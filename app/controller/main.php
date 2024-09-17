7b7492ef7a6cc8b35c8d302b21e8a5464a479978
YroDevGit <tyronemalocon@gmail.com>
2024-09-17 22:28:03 +0800

    tyroneleeemz 111

--- app/controller/main.php
+++ app/controller/main.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends Yros{
 
     public function __construct() {
-        parent::__construct();
+        parent::__construct(); //main class
         $YROS = &Yros::get_instance();
     }
 
