d4c9b37ea0a3d373cc7242cb4ab1c252f2de59e6
YroDevGit <tyronemalocon@gmail.com>
2024-09-24 06:42:16 +0800

    modified:   app/config/routes.php new file:   app/system/functions/yros_command.php modified:   yros

---
+++ app/system/functions/yros_command.php
@@ -0,0 +1,78 @@
+<?php 
+
+/**
+ * Yros PHP framework
+ * 
+ */
+
+if (PHP_SAPI !== 'cli') {
+    echo "This script should only be run from the command line.";
+    exit(1);
+}
+
+$arguments = $argv;
+$route = isset($arguments[1]) ? $arguments[1] : '';
+$filename = isset($arguments[2]) ? $arguments[2] : '';
+$filelower = strtolower($filename);
+if($route==null ||$route ==""){
+    runDev();
+}
+else{
+    $routelower = strtolower($route);
+    if($routelower == "make_api"||$routelower == "+api" || $routelower == "m_a" || $routelower == "make_controller"||$routelower == "+controller" || $routelower == "m_c" || $routelower == "make_model"||$routelower == "+model" || $routelower == "m_m"){
+            if($filename==""||$filename==null){
+                echo "No file to create, please add filename";exit;
+            }
+            else if($filelower == "api" || $filelower == "controller" || $filelower == "controllers" || $filelower == "public" || $filelower == "views"){
+                echo "❌ File not created, Filename '$filename' is not valid.! , try another file name.";exit;
+            }
+            else{
+                $cmnd = strtolower($route);
+                if($cmnd == "make_controller"|| $cmnd == "+controller" || $cmnd == "m_c"){
+                    $createcontroller = addController($filename);
+                    if($createcontroller==200){
+                        echo "\n✅ Controller $filename created.\nOpen @: app/controller/$filename.php\n\n";exit;
+                    }
+                    elseif($createcontroller==-1){
+                        echo "❌ Error";exit;
+                    }
+                    else{
+                        echo "❌ ERROR:: Controller: filename is already exist.!";exit;
+                    }
+                }
+                else if($cmnd == "make_model"|| $cmnd == "+model" || $cmnd == "m_m"){
+                    $createcontroller = addModel($filename);
+                    if($createcontroller==200){
+                        echo "\n✅ Model $filename created.\nOpen @: app/model/$filename.php\n\n";exit;
+                    }
+                    elseif($createcontroller==-1){
+                        echo "❌ Error";exit;
+                    }
+                    else{
+                        echo "❌ ERROR:: Model: filename is already exist.!";exit;
+                    }
+                }
+                else if($cmnd == "make_api" || $cmnd == "+api" || $cmnd == "m_a"){
+                    $createcontroller = addApi($filename);
+                    if($createcontroller==200){
+                        echo "\n✅ Api created.\nOpen @: app/api/$filename.php\n\n";exit;
+                    }
+                    elseif($createcontroller==-1){
+                        echo "❌ Error";exit;
+                    }
+                    else{
+                        echo "❌ ERROR:: API Filename already exist.!";exit;
+                    }
+                }
+            }
+    }
+    elseif($route=="run"||$route=="RUN"){
+        runDev();
+    }
+    else{
+        echo "❌ Invalid command";
+    }
+}
+
+
+?>
