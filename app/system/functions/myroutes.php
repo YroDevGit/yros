5c55e2ed990262de78d2594e04c188344f8c475c
YroDevGit <tyronemalocon@gmail.com>
2024-10-28 14:22:15 +0800

    new update 1.2.1

--- app/system/functions/myroutes.php
+++ app/system/functions/myroutes.php
@@ -2,7 +2,8 @@
 
 include_once "app/system/extras/Routes.php";
 new Routes($routes);
-include "access/routes/web_routes.php";
+include "access/routes/_default.php";
+include "access/routes/custom_routes.php";
 
 
 ?>
