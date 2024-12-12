170b882a3632bae3ab764ad532f41f4fdccdb30a
Tyrone <tyrone@clearmindai.com>
2024-12-12 16:09:06 +0800

    add requested with api

--- app/system/Api.php
+++ app/system/Api.php
@@ -82,7 +82,7 @@ class Api {
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
     }
 
    
