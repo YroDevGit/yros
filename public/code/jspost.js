19e0f48167b77e36832231499be6a1478a3f53aa
Tyrone <tyrone@clearmindai.com>
2025-01-06 13:52:58 +0800

    plain jspost

--- public/code/jspost.js
+++ public/code/jspost.js
@@ -42,7 +42,45 @@ async function jspost(url, data, headers = { 'Content-Type': 'application/json' 
             code: -1,
             status: 'error',
             message: 'Error',
+            data: JSON.stringify(data),
             error: error.message || error,
+            allerror: error
+        };
+    }
+
+    return ret;
+}
+
+async function jspost_plain(url, data, headers = { 'Content-Type': 'application/json' }) {
+    let ret = null;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            ret = result;
+        } else {
+            ret = {
+                code: -1,
+                status: 'error',
+                message: `HTTP error! status: ${response.status}`,
+                error: `HTTP error! status: ${response.status}`,
+            };
+            console.error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        ret = {
+            code: -1,
+            status: 'error',
+            message: 'Error',
+            data: JSON.stringify(data),
+            error: error.message || error,
+            allerror: error
         };
     }
 
@@ -67,6 +105,39 @@ async function jsget(url, headers = { 'Content-Type': 'application/json' }) {
                 data: result,
                 result: result,
             };
+        } else {
+            ret = {
+                code: -1,
+                status: 'error',
+                message: `HTTP error! status: ${response.status}`,
+                error: `HTTP error! status: ${response.status}`,
+            };
+            console.error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        ret = {
+            code: -1,
+            status: 'error',
+            message: 'Error',
+            error: error.message || error,
+        };
+    }
+
+    return ret;
+}
+
+async function jsget_plain(url, headers = { 'Content-Type': 'application/json' }) {
+    let ret = { code: -1, status: 'error', message: 'error message' };
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: headers,
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            ret = result;
         } else {
             ret = {
                 code: -1,
@@ -130,6 +201,42 @@ async function jsput(url, data, where = {}, headers = { 'Content-Type': 'applica
     return ret;
 }
 
+async function jsput_plain(url, data, where = {}, headers = { 'Content-Type': 'application/json' }) {
+    let ret = { code: -1, status: 'error', message: 'error message' };
+    const queryParams = new URLSearchParams(where).toString();
+    const fullUrl = queryParams ? `${url}?${queryParams}` : url;
+
+    try {
+        const response = await fetch(fullUrl, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            ret = result;
+        } else {
+            ret = {
+                code: -1,
+                status: 'error',
+                message: `HTTP error! status: ${response.status}`,
+                error: `HTTP error! status: ${response.status}`,
+            };
+            console.error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        ret = {
+            code: -1,
+            status: 'error',
+            message: 'Error',
+            error: error.message || error,
+        };
+    }
+
+    return ret;
+}
+
 
 function get_form_data(id){
     const form = document.querySelector(`#${id}`); 
@@ -163,6 +270,18 @@ function on_click(id, callable) {
     form.onclick = function (event) {
         callable(event); 
     };
+}
+
+function filter_json_array(data, column, contains, wildcard = "%") {
+    let filteredData;
+    
+    if (wildcard === "%") {
+        filteredData = data.filter(item => item[column].includes(contains));
+    } else {
+        filteredData = data.filter(item => item[column] === contains);
+    }
+    
+    return filteredData;
 }
 
 
