502e66e635ce1142c77c7190aabd4c61576d945f
Tyrone <tyrone@clearmindai.com>
2025-01-16 09:57:18 +0800

    allow-headers all

--- app/config/api_config.php
+++ app/config/api_config.php
@@ -17,7 +17,7 @@ $api_config['api_default_headers'] = [ // default api headers
     "Content-Type: application/json",
     "Access-Control-Allow-Origin: *",
     "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"
+    "Access-Control-Allow-Headers: *"
 ];
 
 
